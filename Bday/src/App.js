17f15918fbcd89c5dbf21f162a52ce5657bab085
Aarathy14 <aarathyganesh14@gmail.com>
2022-03-26 15:59:26 +0530

    day

---
+++ Bday/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Purple from './Purple'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Purple/>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ Bday/src/Purples.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class Purples extends Component {
+    state={
+        showInfo:false
+    }
+    handleshow=(e)=>{this.setState({showInfo:!this.state.showInfo})}
+  render() {
+    return (
+      <div className='b'>
+          <div className='shadow-lg bg-gray'><h3>{this.props.title}
+           <button className='d  rounded-circle' onClick={this.handleshow}>{this.state.showInfo?'-':'+'}
+           </button></h3></div>
+          {
+              this.state.showInfo &&<div className='t'>
+              {this.props.info}</div>
+          }
+      </div>
+    )
+  }
+}
+
+export default Purples
